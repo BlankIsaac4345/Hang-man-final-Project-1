b57ad96c8e5efba4b7cb4d62872230a7edf81c52
BlankIsaac4345 <isaacml4345@gmail.com>
2026-05-01 18:41:54 +0000

    Update 2

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let mysteryWord = "LAMBORGHINI";
 
 function checkGuess() {
     let guessedLetter = document.getElementById("letter-input").value;
+    guessedLetter = guessedLetter.toUpperCase();
     if (mysteryWord.includes(guessedLetter)) {
         for (let i = 0; i < mysteryWord.length; i++) {
         if (mysteryWord[i] === guessedLetter) {
@@ -11,8 +12,8 @@ function checkGuess() {
         }
     }
     } else {
-        // Letter is wrong
+        document.getElementById("wrong-list").innerText += guessedLetter + " ";
     }
-    document.getElementById("guess-button").addEventListener("click", checkGuess);
-    console.log("Click")
+    document.getElementById("letter-input").value = "";
+    
 }
